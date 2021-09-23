56999030072399fc30dfcb7671623fc0c770e1e3
magus <martingramajo08@gmail.com>
2021-09-23 02:27:34 -0300

    agrego los state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import React from 'react';
 import './App.css';
+import Form from './components/Form';
 import Header from './components/Header';
+import { useState } from 'react'
 
-const App = ()=>  {
+const App = () => {
+
+  //State del input
+  const [input, setInput] = useState("");
+
+  //State lista de to do
+  const [todos, setTodos] = useState([]);
   return (
     <div className="container">
       <div className="app-wrapper">
         <div>
           <Header />
+        </div>
+        <div>
+          <Form
+            input={input} //pasamos el state por props
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+
+          />
+
         </div>
 
       </div>
